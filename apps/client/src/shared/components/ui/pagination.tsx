95cc64b20c0604a9ba3e561414d9fe9f241251f1
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2026-01-09 18:00:54 +0530

    fix: ui components imports and deps

--- apps/client/src/shared/components/ui/pagination.tsx
+++ apps/client/src/shared/components/ui/pagination.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react";
-
 import { cn } from "@/lib/utils";
-import { ButtonProps, buttonVariants } from "@/shared//components/ui/button";
+import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react";
+import { type ButtonProps } from "@/shared/components/ui/button";
+import { buttonVariants } from "@/lib/constants/shadcn.constants";
 
 const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
   <nav
@@ -36,7 +36,7 @@ const PaginationLink = ({ className, isActive, size = "icon", ...props }: Pagina
     aria-current={isActive ? "page" : undefined}
     className={cn(
       buttonVariants({
-        variant: isActive ? "outline-solid" : "ghost",
+        variant: isActive ? "outline" : "ghost",
         size,
       }),
       className,
